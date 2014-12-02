ac8c587497d82420cb691c775b08b33520c38888
Svetlana <svetlana.magnifico@gmail.com>
2014-12-02 10:28:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemmoApp::Application.config.secret_key_base = 'dbbd76880dbd2b5e3fc43afdf50e06507e816cad31fd3da141be98b00e7300fbf5d94c43e9936ff93462746ad4c72d97fe7cf93938e6eb0dfd8410f07091747c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemmoApp::Application.config.session_store :cookie_store, key: '_demmo_app_session'
